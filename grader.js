bdb2034a7a56a62dcc8eca243f6bd208aaa27c82
Ubuntu <ubuntu@ip-172-31-20-188.us-west-2.compute.internal>
2015-12-16 21:40:36 +0000

    Updated grader.js

--- grader.js
+++ grader.js
@@ -27,14 +27,13 @@ var cheerio = require('cheerio');
 var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "http://still-waters-8622.herokuapp.com/";
 
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        process.exit(1); 
     }
     return instr;
 };
